0569ce1a178c1d476d34186612db26c446eef20e
Bogdan Gontar <b.hontar@gmail.com>
2026-04-10 14:11:30 +0100

    added railway config files

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+answer = 50
+
+@app.route("/")
+def home():
+    return '''
+        <h1>Guess a number between 1 and 50</h1>
+        <form action="/guess" method="post">
+            <input type="number" name="guess">
+            <button type="submit">Guess</button>
+        </form>
+    '''
+
+@app.route("/guess", methods=["POST"])
+def guess():
+    user_guess = int(request.form["guess"])
+    if user_guess < answer:
+        return "<p>Too low!</p><a href='/'>Try again</a>"
+    elif user_guess > answer:
+        return "<p>Too high!</p><a href='/'>Try again</a>"
+    else:
+        return "<p>Correct!</p>"
+
+app.run(debug=True, port=5001)
